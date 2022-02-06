fd1d914a561985d67583a34f41988a4d5b137f1c
Bryan Carter <deloreanz@gmail.com>
2022-02-05 16:40:07 -0800

    init scaffold of contract and compile tooling

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.3",
+  paths: {
+    root: './src/evm',
+    // sources: './src/evm/contracts',
+    // artifacts: './src/evm/artifacts',
+  }
+};
