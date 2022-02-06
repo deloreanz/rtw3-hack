a6c49e3592068343777626489edfe47bf118ec51
ycuevas <yaricuevas2007@gmail.com>
2022-02-05 21:56:20 -0800

    Update styling

--- src/App.js
+++ src/App.js
@@ -5,18 +5,88 @@ import Game from './components/Game';
 import './App.css';
 import ActiveGames from './components/Active';
 import { useWallet } from './providers/WalletProvider.js';
+import Button from '@mui/material/Button';
+import Modal from './components/Modal';
+import { styled } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
 
 
-
+const Input = styled('input')({
+  display: 'none',
+});
   
-function App() {
+const App = () => {
   const { loginProvider, signer, address, account, accounts, connect, isConnected, balances: coinBalances, network, networkType, networkId, getNetwork } = useWallet();
+  const [modalOpen, setModalOpen] = useState('');
+
+  const state = {
+    modal: false,
+    name: "",
+    modalInputName: ""
+  };
   
+  const openModal = (isModalOpen) => {
+    console.log('ismodalOpen', isModalOpen);
+    setModalOpen(isModalOpen);
+    state.modal = modalOpen
+    console.log('modalOpen: ', modalOpen);
+  }
+
+  const modalClose = (isModalOpen) => {
+    console.log('ismodalOpen', isModalOpen);
+    setModalOpen(isModalOpen);
+    state.modal = modalOpen
+  }
+
   return (
     <div className="App">
-      <Header />
+      <Header openModal={openModal} />
       <Game />
-      <ActiveGames></ActiveGames>
+      {modalOpen && <Modal handleClose={(e) => modalClose(false)}>
+          <h2>New Game</h2>
+          <div className="form-group">
+            <div>
+              <label>Name:</label>
+              <input
+                type="text"
+                value={state.modalInputName}
+                name="modalInputName"
+                onChange={(e) => this.handleChange(e)}
+                className="form-control"
+              />
+            </div>
+            <div>
+              <label for="providers">Choose a provider:</label>
+
+              <select name="providers" id="providers">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+              </select>
+            </div>
+          </div>
+          <div className="form-group">
+            <Button variant="contained" disabled onClick={(e) => this.handleSubmit(e)}>
+              Mint NFT
+            </Button>
+          </div>
+          <div>
+            <label htmlFor="contained-button-file">
+              <Input accept="image/*" id="contained-button-file" multiple type="file" />
+              <Button variant="contained" component="span">
+                Upload
+              </Button>
+            </label>
+            <label htmlFor="icon-button-file">
+              <Input accept="image/*" id="icon-button-file" type="file" />
+              <IconButton color="primary" aria-label="upload picture" component="span">
+                <PhotoCamera />
+              </IconButton>
+            </label>
+          </div>
+        </Modal>}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,38 @@
 import React from "react";
 import './Header.css';
 import logo from '../logo-risk.png';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import { useState } from 'react';
 
-const Header = () => {
+const Header = ({openModal}) => {
+  const state = {
+    modal: false,
+    name: "",
+    modalInputName: ""
+  };
+
+  const data = "This is data from Child Component to the Parent Component."
+
+  // const modalOpen = (e) => {
+  //   // this.setState({ modal: true });
+  //   console.log("hello", e);
+  // }
+
   return (
     <header className="header">
-      <div className="logo-container">
-        <img className="logo" src={logo} alt="Logo" />;
-      </div>
+      <Grid className="header-container" container spacing={2}>
+        <Grid item xs={8}>
+          <div className="logo-container">
+            <img className="logo" src={logo} alt="Logo" />;
+          </div>
+        </Grid>
+        <Grid item xs={4}>
+          <div>
+            <Button variant="outlined" onClick={e => openModal(true)}>Start New Game</Button>
+          </div>
+        </Grid>
+      </Grid>
     </header>
   );
 }
